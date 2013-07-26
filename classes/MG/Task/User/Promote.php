1ab8d6c2ab3621c5d97297ed3d1ad6c2ae2ad284
Hinton <hinton.oscar@gmail.com>
2013-07-26 16:43:53 +0200

    Improved the documentation of User promote task. Fixed some code standard issues.

--- classes/MG/Task/User/Promote.php
+++ classes/MG/Task/User/Promote.php
@@ -2,6 +2,23 @@
 /**
  * Task to promote a user to admin
  *
+ * Available config options are:
+ *
+ *  --username=username
+ *
+ *  This is the username of the user you wish to promote.
+ *
+ *  --type=id
+ *
+ *  How you wish to define roles, use either name or id.
+ *  This value is defaulted to `id`
+ *
+ *  --roles=admin
+ *
+ *  This is the role(s) you wish to promote the user to. It can either be a
+ *  single digit, string or multiple separated by a comma.
+ *  This value defaults to `admin`
+ *
  * @package    MG/User
  * @category   Task
  * @author     Kohana Team
@@ -17,13 +34,14 @@ class MG_Task_User_Promote extends Minion_Task
     );
 
 	/**
-	 * Promote a
+	 * Task to promote a user to admin
 	 *
 	 * Accepts the following options:
 	 *  - username: the user you want to promote
 	 *  - type: [id, name] How you'll assign the role (defaults to id)
 	 *  - roles: a string(single) or multiple(separated by a comma) of {type} role(s) (defaults to admin)
 	 *
+	 * @param array $params
 	 * @return null
 	 */
 	protected function _execute(array $params)
@@ -32,24 +50,27 @@ class MG_Task_User_Promote extends Minion_Task
 			->where('username', '=', $params['username'])
 			->find();
 
-		if(!$user->loaded()) {
-			echo 'error loading user ' . $params['username'];
+		if ( ! $user->loaded())
+		{
+			echo 'Unable to load user "'.$params['username'].'"'.PHP_EOL;
 		}
 		else
 		{
 			$roles = explode(',', $params['roles']);
 
-			foreach($roles as $role) {
-				if($params['type'] == 'name') {
+			foreach ($roles as $role)
+			{
+				if ($params['type'] == 'name')
+				{
 					$role = array('name' => $role);
 				}
 
-				if(!$user->has('roles', $role))
+				if ( ! $user->has('roles', $role))
 				{
 					$user->add('roles', $role);
 				}
 			}
-			echo 'roles assigned';
+			echo 'The user "'.$params['username'].'" was assigned the role(s) "'.$params['roles'].'"'.PHP_EOL;
 		}
 	}
 
--- classes/Task/User/Promote.php
+++ classes/Task/User/Promote.php
@@ -1,3 +1,28 @@
 <?php defined('SYSPATH') OR die('No direct script access.');
-
+/**
+ * Task to promote a user to admin
+ *
+ * Available config options are:
+ *
+ *  --username=username
+ *
+ *  This is the username of the user you wish to promote.
+ *
+ *  --type=id
+ *
+ *  How you wish to define roles, use either name or id.
+ *  This value is defaulted to `id`
+ *
+ *  --roles=admin
+ *
+ *  This is the role(s) you wish to promote the user to. It can either be a
+ *  single digit, string or multiple separated by a comma.
+ *  This value defaults to `admin`
+ *
+ * @package    MG/User
+ * @category   Task
+ * @author     Modular Gaming Team
+ * @copyright  (c) 2012-2013 Modular Gaming Team
+ * @license    BSD http://modulargaming.com/license
+ */
 class Task_User_Promote extends MG_Task_User_Promote {}
